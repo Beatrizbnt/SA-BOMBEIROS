37ddf47d09d9dd0e9bed3c3a6f2b91680e7c2592
Beatriz Bennert <beatrizbennert45@gmail.com>
2023-10-18 22:23:23 -0300

    continuação principal

--- Pages/scripts/principal.js
+++ Pages/scripts/principal.js
@@ -1,18 +1,19 @@
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+// function openNav() {
+//   document.getElementById("myNav").style.height = "100%";
   
-  // Adicione um evento de clique a todos os itens do menu para fechar o menu quando um item é selecionado
-  var menuItems = document.querySelectorAll(".overlay-content a");
-  menuItems.forEach(function(item) {
-      item.addEventListener("click", function() {
-          closeNav();
-      });
-  });
-}
+  
+//   var menuItems = document.querySelectorAll(".overlay-content a");
+//   menuItems.forEach(function(item) {
+//       item.addEventListener("click", function() {
+//           closeNav();
+//       });
+//   });
+// }
 
-function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
-}
+// function closeNav() {
+//   document.getElementById("myNav").style.height = "0%";
+// }
+
 
 
 
@@ -42,17 +43,3 @@ function closeNav() {
     }
   }
 
-
-// Adiciona um evento de mudança aos inputs de sexo
-const femininoInput = document.getElementById('feminino');
-const gestanteDiv = document.getElementById('gestanteDiv');
-
-femininoInput.addEventListener('change', function() {
-  // Se o sexo feminino for selecionado, mostra o input de gestante
-  if (femininoInput.checked) {
-    gestanteDiv.style.display = 'block';
-  } else {
-    // Se o sexo masculino for selecionado, esconde o input de gestante
-    gestanteDiv.style.display = 'none';
-  }
-});
